7c4e396842eb62d591d90ba555652653f8d0b739
Richard Chan <richardchanme@gmail.com>
2017-09-18 22:39:55 -0700

    video playing

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,47 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+	navigator.mediaDevices.getUserMedia({ video: true, audio: false})
+	.then(localMediaStream => {
+	video.src = window.URL.createObjectURL(localMediaStream);
+	video.play();
+	})
+	.catch(err => {
+		console.log(`Error!!!`, err);
+	});
+}
+
+function paintToCanvas() {
+	const width = video.videoWidth;
+	const height = video.videoHeight;
+	canvas.width = width;
+	canvas.height = height;
+
+	return setInterval(() => {
+		ctx.drawImage(video, 0, 0, width, height);
+
+		let pixels = ctx.getImageData(0, 0, width, height);
+
+		pixels = rgbSplit(pixels);
+
+		ctx.putImageData(pixels, 0, 0);
+	}, 16);
+}
+
+function takePhoto() {
+	snap.currentTime = 0;
+	snap.play();
+
+	const data = canvas.toDataURL('image/jpeg');
+	const link = document.createElement('a');
+	link.href = data;
+	link.setAttribute('download', 'handsome');
+	link.innerHTML = `<img src="${data}" alt="Handsome man" />`;
+	strip.insertBefore(link, strip.firstChild);
+}
+
+getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
